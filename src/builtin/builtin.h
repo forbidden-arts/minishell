71aeebdf6783d2136ab9eaef4326ad4e9f372853
Tuomas <tuomas.jaasalo@alceon.fi>
2023-03-15 14:24:28 +0200

    Added builtin protos

--- src/builtin/builtin.h
+++ src/builtin/builtin.h
@@ -6,13 +6,20 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 13:27:38 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/03/15 13:29:21 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2023/03/15 14:24:07 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_H
 # define BUILTIN_H
 
+void	builtin_exit(char **argv);
+void	builtin_echo(char **argv);
+void	builtin_cd(char **argv);
+void	builtin_pwd(char **argv);
+void	builtin_export(char **argv);
+void	builtin_unset(char **argv);
+void	builtin_env(char **argv);
 void	builtin_exit(char **argv);
 
 #endif
