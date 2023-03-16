2a0aa4217fe0579bff11ce0f98519509742c991c
Tuomas <tuomas.jaasalo@alceon.fi>
2023-03-16 11:50:33 +0200

    Added missing defines

--- src/ft/ft.h
+++ src/ft/ft.h
@@ -6,7 +6,7 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 11:36:26 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/03/16 11:38:11 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2023/03/16 11:50:15 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 
 # include <stddef.h>
 # include "bool.h"
+
+# define ERR_INVALID "The string did not consist solely of digit characters."
+# define ERR_TOOSMALL "The result was smaller than the provided minimum value."
+# define ERR_TOOLARGE "The result was larger than the provided maximum value."
 
 BOOL	ft_isspace(char c);
 size_t	ft_strspn(const char *str, const char *charset);
