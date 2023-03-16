edc850ca8202fd0aba3021d667f4f69b6b836fcf
Dan Palmer <dpalmer@student.hive.fi>
2023-03-16 12:18:25 +0200

    exit v2.0

--- src/builtin/builtin_exit.c
+++ src/builtin/builtin_exit.c
@@ -3,20 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 13:27:19 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/03/15 17:10:03 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2023/03/16 12:15:54 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
+#include "ft.h"
 #include "builtin.h"
 
-void	builtin_exit(char **argv)
+static int	int_max(void)
 {
-	(void)argv;
+	return ((int)(((unsigned int)-1) / 2));
+}
+
+static int	int_min(void)
+{
+	return ((int)(((((unsigned int)-1) / 2) + 1)));
+}
+
+void	builtin_exit(char *arg_str)
+{
+	int		exit_code;
+	char	*err_str;
+
+	err_str = NULL;
+	if (!arg_str)
+		exit_code = EXIT_SUCCESS;
+	exit_code = ft_strtonum(arg_str, int_min(), int_max(), &err_str);
+	printf("exit\n");
+	if (err_str)
+	{
+		exit_code = EXIT_FAILURE;
+		printf("minishell: exit: %s: numeric argument required", arg_str);
+	}
 	free_line();
-	exit(EXIT_SUCCESS);
+	exit(exit_code);
 }
