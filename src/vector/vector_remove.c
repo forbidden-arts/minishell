2c49fd5cea2dc0653ab05786b3b45be88cf81d9f
Tuomas <tuomas.jaasalo@alceon.fi>
2023-03-20 17:41:58 +0200

    Fix #18

--- src/vector/vector_remove.c
+++ src/vector/vector_remove.c
@@ -6,10 +6,11 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 03:26:38 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/03/20 14:32:51 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2023/03/20 17:41:24 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
 #include "libft.h"
 #include "vector.h"
 
@@ -17,13 +18,15 @@ void	vector_swap_remove(t_vector *self, size_t index)
 {
 	char	*data;
 	char	*last;
+	size_t	last_idx;
 
-	if (index == self->length - 1)
-		ft_bzero(self->buffer, self->elem_size);
+	last_idx = self->length - 1;
+	if (index == last_idx)
+		ft_bzero(&self->buffer[self->elem_size * index], self->elem_size);
 	else
 	{
 		data = &self->buffer[self->elem_size * index];
-		last = &self->buffer[self->elem_size * (self->length - 1)];
+		last = &self->buffer[self->elem_size * last_idx];
 		ft_memcpy(data, last, self->elem_size);
 	}
 	self->length--;
