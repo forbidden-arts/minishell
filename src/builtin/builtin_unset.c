a34fd3ab5225ffa4d89940036df1f4e6a31e537c
Tuomas <tuomas.jaasalo@alceon.fi>
2023-03-27 15:30:18 +0300

    Handle multiple arguments in unset

--- src/builtin/builtin_unset.c
+++ src/builtin/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:09:09 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/03/23 22:03:42 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2023/03/27 15:29:59 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,29 @@
 #include "shell.h"
 #include "builtin.h"
 
+static int	_builtin_unset(char *name);
+
 int	builtin_unset(t_vector *argv)
 {
-	char	*arg;
+	size_t	idx;
+	int		status;
 
-	if (argv->length <= 1)
-		return (ERR_INVALID_ARGS);
-	arg = *(char **)vector_get(argv, 1);
-	if (!is_name(arg, (size_t)-1))
+	idx = 1;
+	status = 0;
+	while (idx < argv->length)
+		status |= _builtin_unset(*(char **)vector_get(argv, idx++));
+	return (status);
+}
+
+static int	_builtin_unset(char *name)
+{
+	if (!is_name(name, (size_t)-1))
 	{
 		write(STDERR_FILENO, "minishell: unset: `", 20);
-		write(STDERR_FILENO, arg, ft_strlen(arg));
+		write(STDERR_FILENO, name, ft_strlen(name));
 		write(STDERR_FILENO, "': not a valid identifier\n", 26);
 		return (ERR_INVALID_ARGS);
 	}
-	env_unset(arg);
+	env_unset(name);
 	return (OK);
 }
